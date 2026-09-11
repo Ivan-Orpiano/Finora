5cb3dc01f41254200473fd234c54f6de5c8be3b6
Ivan Ritchel Orpiano <147624545+Ivan-Orpiano@users.noreply.github.com>
2026-09-11 18:31:00 +0800

    be: applied CRUD for the attributes

--- finora_tracker/api/views.py
+++ finora_tracker/api/views.py
@@ -1,4 +1,18 @@
 from django.shortcuts import render
+from django.db.models import Sum
+from rest_framework import permissions, status, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Category, Expense, SavingsContribution, SavingsGoal
+from .permissions import IsOwner
+from .serializers import (
+    CategorySerializer,
+    ExpenseSerializer,
+    SavingsContributinSerializer,
+    SavingsGoalSerializer,
+)
+from .utils import PERIOD_CHOICES, annotate_period
+
+
 
